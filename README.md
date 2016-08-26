eb4fb16e2217f4f98ac6c87a23f2879d4136976e
James Garzón <james.garzon@udea.edu.co>
2016-08-26 09:56:55 -0500

    Se crea primera prueba funcionando :3

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # PSP-program2
 
 ###Procedimiento de instalación
-*npm install -g nodemon
-*npm install
+* npm install -g nodemon
+* npm install
 
 ###Para correr el proyecto
-*npm start
+* npm start
+
 
 **abrir navegador en el puerto 8080**
---
+++ server/test/statistic.js
@@ -0,0 +1,38 @@
+process.env.NODE_ENV = 'test';
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Books', () => {
+    beforeEach((done) => {
+        // Book.remove({}, (err) => {
+           done();
+        // });
+    });
+
+  /*
+  * Test the /POST route
+  */
+  describe('/POST book', () => {
+      it('it should not POST a book without pages field', (done) => {
+        let numbers = [4,4,4,4,];
+        chai.request(server)
+            .post('/statistics')
+            .send(numbers)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('number');
+                res.body.should.be.eql(4);
+                // res.body.should.have.property('errors');
+                // res.body.errors.should.have.property('pages');
+                // res.body.errors.pages.should.have.property('kind').eql('required');
+              done();
+            });
+      });
+
+  });
+});
