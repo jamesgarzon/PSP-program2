fa26b23982765c0679815774b29e08b5a2718cac
James Garzón <james.garzon@udea.edu.co>
2016-09-02 19:05:05 -0500

    Se implementa la cantidad de decimales para cada prueba y se hacen los casos de prueba para B1 y B0 de PSP

--- server/libraries/statistics.js
+++ server/libraries/statistics.js
@@ -34,7 +34,7 @@ function Statistics() {
     return covariance;
   }
 
-  function getBZero(numbers){
+  function getBZero(numbers, decimals){
     var bZero = 0;
     var x = [];
     var y = [];
@@ -43,11 +43,13 @@ function Statistics() {
       y.push(number[1]);
     });
     bZero = getMean(y) - getBOne(numbers)*getMean(x);
-    bZero = Math.round(bZero * 1000)/1000;
+    if (decimals) {
+      bZero = Math.round(bZero * (Math.pow(10, decimals)))/(Math.pow(10, decimals));
+    }
     return bZero;
   }
 
-  function getBOne(numbers){
+  function getBOne(numbers, decimals){
     var bOne = 0;
     var x = [];
     var y = [];
@@ -61,9 +63,59 @@ function Statistics() {
     });
     bOne -= numbers.length * getMean(x) * getMean(y);
     bOne /= xPow - numbers.length * Math.pow(getMean(x),2);
-    bOne = Math.round(bOne * 1000)/1000;
+    if (decimals) {
+      bOne = Math.round(bOne * (Math.pow(10, decimals)))/(Math.pow(10, decimals));
+    }
     return bOne;
   }
+
+  // function getBOne(numbers){
+  //   let n = numbers.length;
+  //   let sum_x = 0;
+  //   let sum_y = 0;
+  //   let sum_xy = 0;
+  //   let sum_xx = 0;
+  //   let bettaOne = 0;
+  //
+  //   numbers.forEach(number=>{
+  //     let x = number[0];
+  //     let y = number[1];
+  //
+  //     sum_x += x;
+  //     sum_y += y;
+  //     sum_xx += x*x;
+  //     sum_xy += x*y;
+  //   })
+  //
+  //   bettaOne = (n*sum_xy - sum_x*sum_y) / (n*sum_xx - sum_x*sum_x);
+  //   bettaOne = Math.round(bettaOne * 1000)/1000;
+  //
+  //   return bettaOne;
+  // }
+
+    //
+    // function getBZero(numbers){
+    //   let n = numbers.length;
+    //   let sum_x = 0;
+    //   let sum_y = 0;
+    //   let sum_xy = 0;
+    //   let sum_xx = 0;
+    //   let bettaZero = 0;
+    //   let bettaOne = 0;
+    //
+    //   numbers.forEach(number=>{
+    //     let x = number[0];
+    //     let y = number[1];
+    //
+    //     sum_x += x;
+    //     sum_y += y;
+    //   })
+    //   bettaOne = getBOne(numbers);
+    //   bettaZero = (sum_y/n) - (bettaOne*sum_x)/n;
+    //   bettaZero = Math.round(bettaZero * 1000)/1000;
+    //
+    //   return bettaZero;
+    // }
 
 }
 module.exports =(Statistics);
--- server/test/libraries/statistics.js
+++ server/test/libraries/statistics.js
@@ -6,81 +6,85 @@ var assert = chai.assert;
 
 // chai.use(chaiHttp);
 
-describe('MEAN', () => {
-    beforeEach((done) => {
-      done();
-    });
+// describe('MEAN', () => {
+//     beforeEach((done) => {
+//       done();
+//     });
+//
+//     describe('getMean with no params', function() {
+//       it('List should start empty', function() {
+//         let statistics = new Statistics();
+//         let mean = statistics.getMean();
+//         assert.equal(mean, null);
+//       });
+//     });
+//
+//     describe('getMean with a number', function() {
+//       it('List should start empty', function() {
+//         let statistics = new Statistics();
+//         let mean = statistics.getMean(5);
+//         assert.equal(mean, null);
+//       });
+//     });
+//
+//     describe('getMean with a letter', function() {
+//       it('List should start empty', function() {
+//         let statistics = new Statistics();
+//         let mean = statistics.getMean('a');
+//         assert.equal(mean, null);
+//       });
+//     });
+//
+//     describe('getMean with a bad array', function() {
+//       it('List should start empty', function() {
+//         let statistics = new Statistics();
+//         let mean = statistics.getMean([4,'a']);
+//         assert.equal(mean, null);
+//       });
+//     });
+//
+//     describe('getMean with good data', function() {
+//       it('List should start empty', function() {
+//         let numbers = [2,3,4,4,5,6,6,7,7,8,10,10];
+//         let statistics = new Statistics();
+//         let mean = statistics.getMean(numbers);
+//         assert.equal(mean, 6);
+//       });
+//     });
+//
+//
+// });
+//
+// describe('Covariance', () => {
+//     beforeEach((done) => {
+//       done();
+//     });
+//
+//     // describe('getCovariance with no params', function() {
+//     //   it('List should start empty', function() {
+//     //     let statistics = new Statistics();
+//     //     let Covariance = statistics.getCovariance();
+//     //     assert.equal(Covariance, null);
+//     //   });
+//     // });
+//
+//     describe('Get covariance with good data', function() {
+//       it('List should start empty', function() {
+//         let numbers = [[2,1],[3,3],[4,2],[4,4],[5,4],[6,4],[6,6],[7,4],[7,6],[8,7],[10,9],[10,10]];
+//         let statistics = new Statistics();
+//         let covariance = statistics.getCovariance(numbers);
+//         assert.equal(covariance, 5.92);
+//       });
+//     });
+//
+//
+// });
 
-    describe('getMean with no params', function() {
-      it('List should start empty', function() {
-        let statistics = new Statistics();
-        let mean = statistics.getMean();
-        assert.equal(mean, null);
-      });
-    });
-
-    describe('getMean with a number', function() {
-      it('List should start empty', function() {
-        let statistics = new Statistics();
-        let mean = statistics.getMean(5);
-        assert.equal(mean, null);
-      });
-    });
-
-    describe('getMean with a letter', function() {
-      it('List should start empty', function() {
-        let statistics = new Statistics();
-        let mean = statistics.getMean('a');
-        assert.equal(mean, null);
-      });
-    });
-
-    describe('getMean with a bad array', function() {
-      it('List should start empty', function() {
-        let statistics = new Statistics();
-        let mean = statistics.getMean([4,'a']);
-        assert.equal(mean, null);
-      });
-    });
-
-    describe('getMean with good data', function() {
-      it('List should start empty', function() {
-        let numbers = [2,3,4,4,5,6,6,7,7,8,10,10];
-        let statistics = new Statistics();
-        let mean = statistics.getMean(numbers);
-        assert.equal(mean, 6);
-      });
-    });
 
 
-});
-
-describe('Covariance', () => {
-    beforeEach((done) => {
-      done();
-    });
-
-    // describe('getCovariance with no params', function() {
-    //   it('List should start empty', function() {
-    //     let statistics = new Statistics();
-    //     let Covariance = statistics.getCovariance();
-    //     assert.equal(Covariance, null);
-    //   });
-    // });
-
-    describe('Get covariance with good data', function() {
-      it('List should start empty', function() {
-        let numbers = [[2,1],[3,3],[4,2],[4,4],[5,4],[6,4],[6,6],[7,4],[7,6],[8,7],[10,9],[10,10]];
-        let statistics = new Statistics();
-        let covariance = statistics.getCovariance(numbers);
-        assert.equal(covariance, 5.92);
-      });
-    });
-
-
-});
-
-describe('BOne', () => {
+//CASOS DE PRUEBA PARA OBTENER BETTA UNO
+// =================================================================
+describe('BettaOne (B1)', () => {
     beforeEach((done) => {
         done();
     });
@@ -89,14 +93,53 @@ describe('BOne', () => {
         it('List should start empty', function() {
             let numbers = [[2,1],[3,3],[4,2],[4,4],[5,4],[6,4],[6,6],[7,4],[7,6],[8,7],[10,9],[10,10]];
             let statistics = new Statistics();
-            let bOne = statistics.getBOne(numbers);
+            let bOne = statistics.getBOne(numbers, 3);
             assert.equal(bOne, 0.986);
+        });
+    });
+
+    describe('Test 1 - PSP PROGRAM', function() {
+        it('Return B1 with the data of the PSP PROGRAM', function() {
+            let numbers = [ [130, 186] , [650, 699], [99, 132], [150, 272], [128, 291], [302, 331], [95, 199], [945, 1890], [368, 788], [961, 1601]];
+            let statistics = new Statistics();
+            let bOne = statistics.getBOne(numbers, 3);
+            assert.equal(bOne, 1.728 );
+        });
+    });
+
+    describe('Test 2 - PSP PROGRAM', function() {
+        it('Return B1 with the data of the PSP PROGRAM', function() {
+            let numbers = [[130, 15.0], [650, 69.9], [99, 6.5], [150, 22.4], [128, 28.4], [302, 65.9], [95, 19.4], [945, 198.7], [368, 38.8], [961, 138.2]];
+            let statistics = new Statistics();
+            let bOne = statistics.getBOne(numbers, 4);
+            assert.equal(bOne, 0.1681 );
+        });
+    });
+
+    describe('Test 3 - PSP PROGRAM', function() {
+        it('Return B1 with the data of the PSP PROGRAM', function() {
+            let numbers = [[163, 186], [765, 699], [141, 132], [166, 272], [137, 291], [355, 331], [136, 199], [1206, 1890], [433, 788], [1130, 1601]];
+            let statistics = new Statistics();
+            let bOne = statistics.getBOne(numbers, 5);
+            assert.equal(bOne, 1.43097 );
+        });
+    });
+
+    describe('Test 4 - PSP PROGRAM', function() {
+        it('Return B1 with the data of the PSP PROGRAM', function() {
+            let numbers = [[163, 15.0], [765, 69.9], [141, 6.5], [166, 22.4], [137, 28.4], [355, 65.9], [136, 19.4], [1206, 198.7], [433, 38.8], [1130, 138.2]];
+            let statistics = new Statistics();
+            let bOne = statistics.getBOne(numbers, 6);
+            assert.equal(bOne, 0.140164 );
         });
     });
 
 });
 
-describe('BOne', () => {
+
+//CASOS DE PRUEBA PARA OBTENER BETTA UNO
+// =================================================================
+describe('BettaOne (B1)', () => {
     beforeEach((done) => {
         done();
     });
@@ -105,8 +148,44 @@ describe('BOne', () => {
         it('List should start empty', function() {
             let numbers = [[2,1],[3,3],[4,2],[4,4],[5,4],[6,4],[6,6],[7,4],[7,6],[8,7],[10,9],[10,10]];
             let statistics = new Statistics();
-            let bZero = statistics.getBZero(numbers);
-            assert.equal(bZero, -0.916);
+            let bettaZero = statistics.getBZero(numbers, 3);
+            assert.equal(bettaZero, -0.917);
+        });
+    });
+
+    describe('Test 1 - PSP PROGRAM', function() {
+        it('Return B1 with the data of the PSP PROGRAM', function() {
+            let numbers = [ [130, 186] , [650, 699], [99, 132], [150, 272], [128, 291], [302, 331], [95, 199], [945, 1890], [368, 788], [961, 1601]];
+            let statistics = new Statistics();
+            let bettaZero = statistics.getBZero(numbers, 2);
+            assert.equal(bettaZero, -22.55 );
+        });
+    });
+
+    describe('Test 2 - PSP PROGRAM', function() {
+        it('Return B1 with the data of the PSP PROGRAM', function() {
+            let numbers = [[130, 15.0], [650, 69.9], [99, 6.5], [150, 22.4], [128, 28.4], [302, 65.9], [95, 19.4], [945, 198.7], [368, 38.8], [961, 138.2]];
+            let statistics = new Statistics();
+            let bettaZero = statistics.getBZero(numbers, 3);
+            assert.equal(bettaZero, -4.039 );
+        });
+    });
+
+    describe('Test 3 - PSP PROGRAM', function() {
+        it('Return B1 with the data of the PSP PROGRAM', function() {
+            let numbers = [[163, 186], [765, 699], [141, 132], [166, 272], [137, 291], [355, 331], [136, 199], [1206, 1890], [433, 788], [1130, 1601]];
+            let statistics = new Statistics();
+            let bettaZero = statistics.getBZero(numbers, 2);
+            assert.equal(bettaZero, -23.92 );
+        });
+    });
+
+    describe('Test 4 - PSP PROGRAM', function() {
+        it('Return B1 with the data of the PSP PROGRAM', function() {
+            let numbers = [[163, 15.0], [765, 69.9], [141, 6.5], [166, 22.4], [137, 28.4], [355, 65.9], [136, 19.4], [1206, 198.7], [433, 38.8], [1130, 138.2]];
+            let statistics = new Statistics();
+            let bettaZero = statistics.getBZero(numbers, 3);
+            assert.equal(bettaZero, -4.604  );
         });
     });
 
